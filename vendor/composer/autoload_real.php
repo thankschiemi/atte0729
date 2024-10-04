c35ef0c9a7ab06c066cc33b010c17a29da020651
EC2 Default User <ec2-user@ip-172-31-40-189.ap-southeast-2.compute.internal>
2024-10-04 08:10:45 +0000

    Added FileUploadController

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitd2e0a434cf90d089e32b1786328d6b50
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitd2e0a434cf90d089e32b1786328d6b50::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
