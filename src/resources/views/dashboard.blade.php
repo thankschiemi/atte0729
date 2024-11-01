a7f1eeab945028bcfe0cfe2fa892b4edb1762912
鈴木智恵美 <thankschimail.gmail.com>
2024-11-01 10:11:41 +0000

    ダッシュボードのログイン完了メッセージを日本語に変更

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -11,12 +11,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    ログインが完了しました
 
                     <!-- アプリへのリンク -->
                     <div class="mt-4">
                         <a href="{{ route('home') }}" class="text-blue-500 underline">
-                            Go to Atte
+                            勤怠管理アプリ Atteへ移動
                         </a>
                     </div>
                 </div>
